646cb00fbe68a3abb53a501f053f836144e5c824
Ryan Gjoraas <ryangjoraas@Ryans-MacBook-Pro.local>
2017-11-27 22:17:07 -0600

    lesson-10 finished

---
+++ Smack/View/AvatarCell.swift
@@ -0,0 +1,26 @@
+//
+//  AvatarCell.swift
+//  Smack
+//
+//  Created by Ryan Gjoraas on 11/27/17.
+//  Copyright © 2017 Gorgeous Running. All rights reserved.
+//
+
+import UIKit
+
+class AvatarCell: UICollectionViewCell {
+    
+    @IBOutlet weak var avatarImg: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setUpView()
+    }
+    
+    func setUpView() {
+        self.layer.backgroundColor = UIColor.lightGray.cgColor
+        self.layer.cornerRadius = 10
+        self.clipsToBounds = true
+    }
+    
+}
